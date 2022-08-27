3267cd5af6af2cffc43380a58bfb7b7a9a63ab04
Agunivers7 <110654906+Agunivers7@users.noreply.github.com>
2022-08-27 12:15:29 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -11,10 +11,10 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 @Client.on_message(filters.private & filters.command(['start']))
 async def start(client, message):
     buttons = [[
-        InlineKeyboardButton('📜 Support Group', url='https://t.me/DxHelpDesk'),
-        InlineKeyboardButton('Update Channel ♻️', url='https://t.me/DX_Botz')
+        InlineKeyboardButton('📜 Support Group', url='https://t.me/Agunivers_backup'),
+        InlineKeyboardButton('Update Channel ♻️', url='https://t.me/Apex_legends_AG')
     ],[
-        InlineKeyboardButton('SouceCode 💡', url='https://github.com/Jijinr/Frwdit')
+        InlineKeyboardButton('Support me', url='https://www.instagram.com/invites/contact/?i=152wmuaexwcia&utm_content=ndyj4ft')
     ]]
     reply_markup = InlineKeyboardMarkup(buttons)
     await client.send_message(
@@ -39,7 +39,7 @@ async def help(client, message):
 @Client.on_message(filters.private & filters.command(['about']))
 async def about(client, message):
     buttons = [[
-        InlineKeyboardButton('💡 SouceCode', url='https://github.com/Jijinr/Frwdit'),
+        InlineKeyboardButton('Source', url='https://t.me/Apex_legends_AG'),
         InlineKeyboardButton('close 🔐', callback_data='close_btn')
     ]]
     reply_markup = InlineKeyboardMarkup(buttons)
